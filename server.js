5f64d7dc1b55fed389a5cd0173cd532921a9dd23
olya19 <titarenko.olga.1997@gmail.com>
2017-07-28 10:57:57 +0300

    added server

---
+++ server.js
@@ -0,0 +1,74 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+
+
+function onGetRequest(req, res){
+  const parsedUrl = url.parse(req.url);
+  // extract URL path
+  let pathname = `.${parsedUrl.pathname}`;
+  console.log(pathname  + ' pathname');
+  // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+  let ext = path.parse(pathname).ext;
+  console.log(ext + ' ext');
+  // maps file extention to MIME typere
+  const MIME_TYPE = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword'
+  };
+
+  fs.exists(pathname, (exist) => {
+    if(!exist) {
+      // if the file is not found, return 404
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    // if is a directory search for index file matching the extention
+    if (fs.statSync(pathname).isDirectory()) {
+      pathname += 'index.html';
+      console.log(pathname);
+      ext = '.html';
+    }
+    // read file from file system
+    fs.readFile(pathname, (err, data) => {
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        // if the file is found, set Content-type and send data
+        console.log(this);
+        res.setHeader('Content-type', MIME_TYPE[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+
+}
+
+http.createServer((req, res) => {
+  console.log(`${req.method} ${req.url}`);
+  // parse URL
+  switch (req.method) {
+    case 'GET': onGetRequest(req, res);
+      break;
+    default: console.log('Can not !');
+
+  }
+
+}).listen(8080);
+
+console.log(`Server listening on port 8080`);
